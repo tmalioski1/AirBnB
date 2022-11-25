8aea07f4db7ecea3b6a1e8098d658944703dbf58
Tyler Malinoski <tmalinoski1@gmail.com>
2022-11-24 22:56:07 -0800

    new stuff

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
-const { restoreUser } = require("../../utils/auth.js");
+const { restoreUser, requireAuth } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
