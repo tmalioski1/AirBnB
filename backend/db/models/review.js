ea398c4fae6f15793fba500a6d1b4b61c0a497e0
Tyler Malinoski <tmalinoski1@gmail.com>
2022-11-28 08:29:25 -0800

    salvage

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   Review.init({
     spotId: {
       allowNull: false,
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER
 
     },
     userId: {
