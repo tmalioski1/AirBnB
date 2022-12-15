26f56b6d265bee060404ded67d9b1c6c66094f88
Tyler Malinoski <tmalinoski1@gmail.com>
2022-12-15 15:07:17 -0800

    setup review

--- frontend/src/components/SingleSpotPage/index.js
+++ frontend/src/components/SingleSpotPage/index.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useParams, NavLink, useHistory } from 'react-router-dom';
 import { getOneSpot, deleteOneSpot } from '../../store/spots';
+import { getAllReviewsForSpot } from '../../store/reviews';
 import './SingleSpotPage.css';
 
 
@@ -13,13 +14,22 @@ const SingleSpotPage = () => {
   const sessionUser = useSelector(state => state.session.user);
   const owner = useSelector(state => state.spots.singleSpot.ownerId);
   const [validationErrors, setValidationErrors] = useState([]);
+  const reviewsObj = useSelector(state => state.reviews.spot);
+  console.log('this is the reviewsObj', reviewsObj)
+  const reviews = Object.values(reviewsObj)
+  console.log('this is the reviews', reviews)
 
   useEffect(() => {
-    dispatch(getOneSpot({spotId}))
+    dispatch(getOneSpot({ spotId }))
   }, [spotId, dispatch])
 
+  useEffect(() => {
+    dispatch(getAllReviewsForSpot({ spotId }))
+  }, [spotId, dispatch])
+
+
   const errors = [];
-  const deleteSpot =  async (e) => {
+  const deleteSpot = async (e) => {
     e.preventDefault();
     if (!sessionUser) {
       errors.push('User must be logged in to delete spot')
@@ -31,34 +41,43 @@ const SingleSpotPage = () => {
       setValidationErrors(errors);
     }
 
-      else {
-    await dispatch(deleteOneSpot(spotId))
-    history.push('/')
+    else {
+      await dispatch(deleteOneSpot(spotId))
+      history.push('/')
     }
-}
+  }
 
- const userValidation = (e) => {
-     if (!sessionUser) {
+  const userValidation = (e) => {
+    if (!sessionUser) {
       e.preventDefault();
       errors.push('User must be logged in to edit spot')
       setValidationErrors(errors);
-     }
- }
+    }
+  }
 
   return (
     <>
-   <h1>{spotsObj.description}</h1>
-   <ul className="errors">
-          {validationErrors.map((error) => (
-            <li key={error}>{error}</li>
-          ))}
-        </ul>
-   <div>
-   <NavLink onClick={userValidation}to={`/spots/${spotId}/edit`}>Edit Your Spot</NavLink>
-   </div>
-   <button onClick={deleteSpot}>Delete Spot</button>
-   <div className='all-reviews-container'>Reviews</div>
-   </>
+      <h1>{spotsObj.description}</h1>
+      <ul className="errors">
+        {validationErrors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <div>
+        <NavLink onClick={userValidation} to={`/spots/${spotId}/edit`}>Edit Your Spot</NavLink>
+      </div>
+      <button onClick={deleteSpot}>Delete Spot</button>
+      <ul className='all-reviews-container'>
+      {
+          reviews.map(review => (
+            <div className = 'review-container' key={review.id}>
+             <li className='review'>{review.review}</li>
+            </div>
+          ))
+        }
+
+      </ul>
+    </>
 
 
   );
--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,11 +1,43 @@
-// import { csrfFetch } from './csrf'
+import { csrfFetch } from './csrf'
+
+const LOAD = 'reviews/LOAD'
+// const LOAD_ONE = 'reviews/LOAD_ONE'
+// const CREATE_ONE = 'reviews/CREATE_ONE'
+// const DELETE_ONE = 'reviews/DELETE_ONE'
+
+const loadAllReviewsForSpot = (reviews) => ({
+  type: LOAD,
+  reviews,
+})
+
+export const getAllReviewsForSpot = (id) => async (dispatch) => {
+    const { spotId } = id
+    console.log('this is the id', id)
+    console.log('this is the spotId', spotId)
+    const response = await csrfFetch(`/api/spots/${spotId}/reviews`);
+    if (response.ok) {
+      const reviews = await response.json();
+      dispatch(loadAllReviewsForSpot(reviews));
+    }
+    return response
+  };
+
 
 
 const initialState = { spot: {}, user: {} }
 
 const reviewsReducer = (state = initialState, action) => {
     switch (action.type) {
+     case LOAD: {
+        const newState = { spot: {}, user: {} }
+        console.log(Array.isArray(action.reviews.Reviews))
+        console.log('this is action.reviews.Reviews', action.reviews.Reviews)
+        action.reviews.Reviews.forEach(review => {
+            newState.spot[review.id] = review
+        })
 
+        return newState;
+     }
 
         default:
           return state
