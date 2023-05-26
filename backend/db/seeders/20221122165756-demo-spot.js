e44e61f509f81f7df8f13f222dc4137c8c673455
Tyler Malinoski <tmalinoski1@gmail.com>
2023-05-25 22:15:20 -0700

    re-seeded a bit

--- backend/db/seeders/20221122165756-demo-spot.js
+++ backend/db/seeders/20221122165756-demo-spot.js
@@ -46,7 +46,94 @@ module.exports = {
         name: "Jane's villa",
         description: 'luxurious',
         price: 55.60
-      }
+      },
+      {
+        ownerId: 3,
+        address: '504 Spring Rd.',
+        city: 'San Diego',
+        state: 'California',
+        country: 'USA',
+        lat: 9.2,
+        lng: 7.3,
+        name: "Jane's seaside villa",
+        description: 'Tremendous seaside views',
+        price: 75.60
+      },
+      {
+        ownerId: 3,
+        address: '505 Water Rd.',
+        city: 'Wilmington',
+        state: 'Tennessee',
+        country: 'USA',
+        lat: 12.2,
+        lng: 7.3,
+        name: "Rustic Canyon Retreat",
+        description: 'Wonderful nature hideaway',
+        price: 65.60
+      },
+      {
+        ownerId: 3,
+        address: '203 Alvin St.',
+        city: 'Little Rock',
+        state: 'Arkansas',
+        country: 'USA',
+        lat: 14.23,
+        lng: 77.30,
+        name: "Cozy Woods Hideway",
+        description: 'A true delight for all nature lovers',
+        price: 69.10
+      },
+
+      {
+        ownerId: 3,
+        address: '907 Viewport Rd.',
+        city: 'Welling',
+        state: 'California',
+        country: 'USA',
+        lat: 20.23,
+        lng: 70.30,
+        name: "Oceanside Getaway",
+        description: 'A paradise for all beach lovers',
+        price: 96.60
+      },
+      {
+        ownerId: 3,
+        address: '123 Broadway Ave.',
+        city: 'New York',
+        state: 'New York',
+        country: 'USA',
+        lat: 10.23,
+        lng: 86.30,
+        name: "Jane's NYC Penthouse",
+        description: 'The best way to enjoy Manhattan',
+        price: 166.60
+      },
+      {
+        ownerId: 3,
+        address: '1245 River Rd.',
+        city: 'Blem',
+        state: 'Oregon',
+        country: 'USA',
+        lat: 23.23,
+        lng: 69.30,
+        name: "Northwest Paradise",
+        description: 'Wonderful nature hideaway',
+        price: 86.60
+      },
+      {
+        ownerId: 3,
+        address: '203 Something Rd.',
+        city: 'Pitman',
+        state: 'New Jersey',
+        country: 'USA',
+        lat: 23.23,
+        lng: 99.30,
+        name: "Jane's Small Town Bungalow",
+        description: 'Wonderful weekend retreat',
+        price: 56.60
+      },
+
+
     ], {});
   },
 
--- backend/db/seeders/20221122171555-demo-spotimage.js
+++ backend/db/seeders/20221122171555-demo-spotimage.js
@@ -23,7 +23,7 @@ module.exports = {
    return queryInterface.bulkInsert(options, [
     {
       spotId: 1,
-      url: 'https://a0.muscache.com/im/pictures/b0c977d9-57ba-4f70-bbb4-8b3cd3ae8dc4.jpg?im_w=1200',
+      url: 'https://a0.muscache.com/im/pictures/db2e7230-23db-4479-b6fe-b50963babeb6.jpg?im_w=1200',
       preview: true
     },
     {
@@ -35,7 +35,42 @@ module.exports = {
       spotId: 3,
       url: 'https://a0.muscache.com/im/pictures/559bdede-53d5-4f42-9851-e952a75409b7.jpg?im_w=1200',
       preview: true
-    }
+    },
+      {
+      spotId: 4,
+      url: 'https://a0.muscache.com/im/pictures/34ca7926-03f2-4713-9a42-1b1ed9fb5c43.jpg?im_w=1200',
+      preview: true
+    },
+      {
+      spotId: 5,
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48246773/original/22a2c42e-108b-4809-b274-ada4f3d6da28.jpeg?im_w=1200',
+      preview: true
+    },
+      {
+      spotId: 6,
+      url: 'https://a0.muscache.com/im/pictures/c88d4356-9e33-4277-83fd-3053e5695333.jpg?im_w=1200',
+      preview: true
+    },
+      {
+      spotId: 7,
+      url: 'https://a0.muscache.com/im/pictures/2e5ce6c9-4935-49ce-891e-e6f7251a8590.jpg?im_w=1200',
+      preview: true
+    },
+      {
+      spotId: 8,
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-49381739/original/ddca4b48-7044-4e29-8978-540dc9415df0.jpeg?im_w=1200',
+      preview: true
+    },
+      {
+      spotId: 9,
+      url: 'https://a0.muscache.com/im/pictures/1e16f2f4-1256-44cb-a0f2-85aa57672a45.jpg?im_w=1200',
+      preview: true
+    },
+      {
+      spotId: 10,
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-46695796/original/a0965aa5-3907-466e-b727-0900e2a7e8c7.jpeg?im_w=1200',
+      preview: true
+    },
    ], {});
 
   },
