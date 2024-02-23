3f23e46e68374c21f0e0fbebc810c5e1c1eed9da
Tyler Malinoski <tmalinoski1@gmail.com>
2024-02-23 14:00:54 -0800

    finished refactoring reviews in store

--- frontend/src/store/bookings.ts
+++ frontend/src/store/bookings.ts
@@ -1,4 +1,8 @@
 import { csrfFetch } from './csrf'
+import { ThunkAction } from 'redux-thunk';
+import { Action } from 'redux';
+
+
 
 interface Booking {
 id: number;
@@ -40,92 +44,91 @@ const deleteBooking = (id: string): {type: typeof DELETE; id: string} => ({
 })
 
 interface BookingsState {
-    booking: {[id: string]: Booking }
+    bookings: {[id: string]: Booking }
 }
 
-export const getCurrentBookings = () => async (dispatch) => {
+export const getCurrentBookings = ():ThunkAction<void, BookingsState, unknown, Action<string>> => async (dispatch) => {
     const response = await csrfFetch('/api/bookings/current')
 
     if(response.ok){
-        const yourBookings = await response.json()
+        const yourBookings: Booking[] = await response.json()
         dispatch(getBookings(yourBookings))
     }
 }
 
-export const makeNewBooking = (spotId, newBooking) => async (dispatch) => {
+export const makeNewBooking = (spotId: number, newBooking: Booking): ThunkAction<Promise<Booking | void>, BookingsState, unknown, Action<string>> => async (dispatch) => {
     const response = await csrfFetch(`/api/spots/${spotId}/bookings`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newBooking)
-    })
-    if(response.ok){
-        const yourNewBooking = await response.json()
-        dispatch(makeBooking(yourNewBooking))
-        return yourNewBooking
-    } else{
-        const error = await response.json()
-        return error
-    }
-}
+        body: JSON.stringify(newBooking),
+    });
 
-export const editYourBooking = (id, booking) => async (dispatch) =>{
+    if (response.ok) {
+        const yourNewBooking: Booking = await response.json();
+        dispatch(makeBooking(yourNewBooking));
+        return yourNewBooking;
+    } else {
+        const error = await response.json();
+        return error;
+    }
+};
+
+export const editYourBooking = (id: number, booking: Booking): ThunkAction<Promise<Booking | void>, BookingsState, unknown, Action<string>> => async (dispatch) => {
     const response = await csrfFetch(`/api/bookings/${id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(booking)
-    })
-    if(response.ok){
-        const editedBooking = await response.json()
-        dispatch(editBooking(editBooking))
-        getCurrentBookings()
-        return editedBooking
+        body: JSON.stringify(booking),
+    });
+
+    if (response.ok) {
+        const editedBooking = await response.json();
+        dispatch(editBooking(editedBooking));
+        dispatch(getCurrentBookings());
+        return editedBooking;
     }
+};
 
 
-}
+export const deleteYourBooking = (id: number): ThunkAction<Promise<Booking | void>, BookingsState, unknown, Action<string>> => async (dispatch) => {
+    const response = await csrfFetch(`/api/bookings/${id}`, {
+        method: 'DELETE',
+    });
 
-
-export const deleteYourBooking = (id) => async (dispatch) => {
-    const response = await csrfFetch(`/api/bookings/${id}`,{
-        method:'DELETE'
-    })
-
-    if(response.ok){
-        const deletedBooking = await response.json()
-        dispatch(deleteBooking(id))
-        return deletedBooking
+    if (response.ok) {
+        const deletedBooking = await response.json();
+        dispatch(deleteBooking(id.toString()));
+        return deletedBooking;
     }
-}
+};
 
 
 
-const initialState = {bookings: {}}
-const bookingsReducer = (state = initialState, action ) => {
-    switch(action.type){
-        case LOAD:{
-            const newState = {...state}
-            const yourBookings = {...state.bookings}
-            action.bookings.Bookings.forEach(booking => yourBookings[booking.id] = booking)
-            newState.bookings = yourBookings
-            return newState
+const initialState: BookingsState = { bookings: {} };
+const bookingsReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        case LOAD: {
+            const newState = { ...state };
+            const yourBookings = { ...state.bookings };
+            action.bookings.forEach((booking: Booking) => (yourBookings[booking.id.toString()] = booking));
+            newState.bookings = yourBookings;
+            return newState;
         }
-        case CREATE:{
-            const newState = {...state}
-            const newBooking = {...state.bookings}
-            newBooking[action.booking.id] = action.booking
-            newState.bookings = newBooking
-            return newState
+        case CREATE: {
+            const newState = { ...state };
+            const newBooking = { ...state.bookings };
+            newBooking[action.booking.id.toString()] = action.booking;
+            newState.bookings = newBooking;
+            return newState;
         }
-        case DELETE:{
-            const newState = {...state}
-            const newBooking = {...state.bookings}
-            delete newBooking[action.id]
-            newState.bookings = newBooking
-            return newState
+        case DELETE: {
+            const newState = { ...state };
+            const newBooking = { ...state.bookings };
+            delete newBooking[action.id];
+            newState.bookings = newBooking;
+            return newState;
         }
-
         default:
-            return state
+            return state;
     }
 }
 
